98c5c0b124323dff69aa085cbf0dcf4bca53d170
Elijah Miller <elijah.miller@gmail.com>
2008-08-29 00:00:31 -0400

    Referring to revision properly.

--- lib/snitch/base.rb
+++ lib/snitch/base.rb
@@ -9,7 +9,7 @@ module Snitch
       config_file = options[:config_file]
       Config.config_file_path = config_file unless config_file.nil?
       @config                 = Config::load
-      @scm = 
+      @revision = 
         case options[:scm] 
         when :git
           GitCommit.new(repository, revision)
@@ -25,7 +25,7 @@ module Snitch
     #   * <tt>:long</tt> is the full commit message along with a list of all changed files. 
     #   * <tt>:short</tt> is a truncated version of the commit message that is less than 140 characters for twitter.
     def commit_message(which=:long)
-      @scm.to_s(which)
+      @revision.to_s(which)
     end
     
     # Adds a service to the services array from a string or symbol of the service name and a hash of attributes.
