a2cd01d06958a5c57c9e71faa0349eff2f65e0e0
Elijah Miller <elijah.miller@gmail.com>
2008-08-29 00:50:41 -0400

    Simplifying campfire login code.

--- lib/snitch/services/campfire.rb
+++ lib/snitch/services/campfire.rb
@@ -4,14 +4,11 @@ class Snitch
   module Services
     class Campfire < Service
             
-      def connection(force=false)
-        if @campfire.nil? || force
-          connection = ::Tinder::Campfire.new(subdomain)
-          connection.login(login, password)
-          raise ServiceLoginError, "Login to Campfire failed" unless connection.logged_in?
-          @campfire = connection.find_or_create_room_by_name(room)
-        end
-        @campfire
+      def connection
+        connection = ::Tinder::Campfire.new(subdomain)
+        connection.login(login, password)
+        raise ServiceLoginError, "Login to Campfire failed" unless connection.logged_in?
+        connection.find_or_create_room_by_name(room)
       end
       
       def tattle(message)
