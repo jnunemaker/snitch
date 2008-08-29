fcd38b7c7bf07b7c8439fad7f592449677c2870a
Elijah Miller <elijah.miller@gmail.com>
2008-08-29 11:58:03 -0400

    Workaround for Tinder bug.

--- lib/snitch/services/campfire.rb
+++ lib/snitch/services/campfire.rb
@@ -14,7 +14,10 @@ class Snitch
       end
       
       def tattle(message)
-        connection.speak(message, :paste => paste)
+        # working around Tinder bug: see http://github.com/jqr/tinder/commit/5183567f060c991b7ed9c986b0594f8a53c2be63
+        speak_options = {}
+        speak_options[:paste] = true if paste
+        connection.speak(message, speak_options)
       end
     end
   end
