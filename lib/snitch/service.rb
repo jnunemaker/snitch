257747924b06a88ef62e52b6c07365eb30dd19ec
Elijah Miller <elijah.miller@gmail.com>
2008-08-29 00:24:47 -0400

    Removing docs from Service.

--- lib/snitch/service.rb
+++ lib/snitch/service.rb
@@ -1,17 +1,11 @@
 class Snitch
-  # Service is the base class for all services. All services should inherit from this class.
   class Service
     attr_reader :base, :attributes
     
     class << self
       
-      # Handy for creating a new instance of a service from the config file. Simply pass in the service name and the attributes for the service and you get a new instance of the service.
-      #
-      #   Snitch::Service.new_from_name(:twitter, {:login => 'jnunemaker', :password => 'secret'})
-      #   # => #<Snitch::Services::Twitter:0x15a6508 @attributes={:login=>"jnunemaker", :password=>"secret"}>
       def new_from_name(s, attributes)
-        service = "Snitch::Services::#{s.to_s.camelize}".constantize
-        service.new(attributes)
+        "Snitch::Services::#{s.to_s.camelize}".constantize.new(attributes)
       end
     end
     
@@ -20,14 +14,6 @@ class Snitch
       @attributes = defaults.merge(attributes || {})
     end
     
-    # Uses method missing to return the value of a key in the attributes hash.
-    # Allows for doing this...
-    #
-    #   service.login
-    #
-    # instead of this...
-    #
-    #   service.attributes[:login]
     def method_missing(method, *args, &block)
       if method.to_s =~ /=$/
         @attributes[method.to_s.chop] = args[0]
