a4b465b15d9ab7bf7c7890205891540f276e0970
Elijah Miller <elijah.miller@gmail.com>
2008-08-29 00:43:09 -0400

    Removing tinder patch.

--- lib/snitch/patches/tinder.rb
+++
@@ -1,9 +0,0 @@
-module Tinder
-  class Campfire
-    def find_or_create_room_by_name(name)
-      room = find_room_by_name(name)
-      room = create_room(name) if room.nil?
-      room
-    end
-  end
-end
