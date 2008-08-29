8cf43ba96af52b584bdb013333ff8addd817dd4c
Elijah Miller <elijah.miller@gmail.com>
2008-08-29 12:38:06 -0400

    Adding tests for Service.new_from_name for Echo and Email.

--- test/unit/service_test.rb
+++ test/unit/service_test.rb
@@ -16,7 +16,9 @@ class ServiceTest < Test::Unit::TestCase
   end
   
   test 'should be able to create new instance from name' do
+    assert_kind_of Snitch::Services::Echo, Snitch::Service.new_from_name(:echo, {})
     assert_kind_of Snitch::Services::Twitter, Snitch::Service.new_from_name(:twitter, {})
     assert_kind_of Snitch::Services::Campfire, Snitch::Service.new_from_name(:campfire, {})
+    assert_kind_of Snitch::Services::Email, Snitch::Service.new_from_name(:email, {})
   end
 end
