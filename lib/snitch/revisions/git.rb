e5c85e9741f44db0b4ad9f9d5ec77ca331403822
Elijah Miller <elijah.miller@gmail.com>
2008-08-29 01:04:57 -0400

    Doing a gem require for mojombo-grit.

--- lib/snitch/revisions/git.rb
+++ lib/snitch/revisions/git.rb
@@ -1,12 +1,8 @@
 class Snitch
   module Revisions
-    # A simple interface class for snitch's services to use.  Requires the mojombo-grit gem
-    #   
-    #   gitcommit = Snitch::GitCommit.new('/var/www/apps/yourapp.git/', 'a9s8d76f9as8d76f9sa...'
-    #   puts gitcommit.author, gitcommit.project # etc, etc, etc.
-    #
     class Git
       include Message
+      gem 'mojombo-grit'
       require 'mojombo-grit'
     
       attr_reader :repository
