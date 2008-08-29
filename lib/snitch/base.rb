3dffd558b1134ef3920271acdb5692fd78a5fd5f
Elijah Miller <elijah.miller@gmail.com>
2008-08-29 00:09:03 -0400

    Removing documentation for Snitch::Base

--- lib/snitch/base.rb
+++ lib/snitch/base.rb
@@ -2,9 +2,6 @@ module Snitch
   class Base
     attr_reader :services, :config
     
-    # Creates a new snitch repository path and a revision.
-    #
-    #   Snitch::Base.new('/var/www/apps/myapp/repos', 102, :config_file => '/home/user/.snitch')
     def initialize(repository, revision, options)
       config_file = options[:config_file]
       Config.config_file_path = config_file unless config_file.nil?
@@ -24,26 +21,14 @@ module Snitch
       end
     end
     
-    # Obtains the commit message from the svnlook class. Takes either <tt>:long</tt> or <tt>:short</tt> as optional parameter. 
-    #   * <tt>:long</tt> is the full commit message along with a list of all changed files. 
-    #   * <tt>:short</tt> is a truncated version of the commit message that is less than 140 characters for twitter.
-    def commit_message(which=:long)
+    def commit_message(which = :long)
       @revision.to_s(which)
     end
     
-    # Adds a service to the services array from a string or symbol of the service name and a hash of attributes.
-    #
-    #   snitch = Snitch::Base.new('/var/www/apps/myapp/repos', 102)
-    #   snitch.use(:twitter, {:login => 'jnunemaker', :password => 'secret'})
     def use(s, attrs = {})
       @services << Service.new_from_name(s, attrs)
     end
     
-    # Sends the commit message to all the added services. The following code would tattle the commit message to twitter.
-    #
-    #   snitch = Snitch::Base.new('/var/www/apps/myapp/repos', 102)
-    #   snitch.use(:twitter, {:login => 'jnunemaker', :password => 'secret'})
-    #   snitch.tattle
     def tattle
       superlong = commit_message(:superlong)
       long = commit_message(:long)
